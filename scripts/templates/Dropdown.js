e498313c9397188bc95d4961fb2c6ec65fa85a52
atmani samira <erp.atmanis@gmail.com>
2024-02-06 10:40:11 +0100

    suite drowpdown

--- scripts/templates/Dropdown.js
+++ scripts/templates/Dropdown.js
@@ -3,6 +3,7 @@ export class DropdownTemplate {
     this.ingredient = ingredient;
     this.appliance = appliance;
     this.ustensiles = ustensiles;
+    this.recetteClickCount = 0;
   }
 
   createElement(category, liste) {
@@ -22,17 +23,12 @@ export class DropdownTemplate {
 
     // l'icône de recherche
     const searchIcon = document.createElement("i");
-    searchIcon.className = "fas fa-search";
-
-    const searchIconListItem = document.createElement("li");
-    searchIconListItem.appendChild(searchIcon);
-    dropdownMenu.appendChild(searchIconListItem);
+    searchIcon.className = "fas fa-search hidden";
 
     const inputElement = document.createElement("input");
     inputElement.className = "dropdown__input";
     inputElement.type = "search";
-    inputElement.placeholder = `Rechercher une recette, un ingrédient,  ${category}`;
-    searchIconListItem.appendChild(inputElement);
+    inputElement.placeholder = `Rechercher une recette, un ingrédient, ${category}`;
 
     // la liste déroulante
     liste.forEach((item) => {
@@ -42,35 +38,47 @@ export class DropdownTemplate {
     });
 
     container.appendChild(dropdownDiv);
+    container.appendChild(searchIcon);
+    container.appendChild(inputElement);
     container.appendChild(dropdownMenu);
     container.appendChild(icone);
 
-    // dropdownMenu.classList.add("hidden");
+    // Initialiser la liste comme cachée
+    dropdownMenu.classList.add("hidden");
+
     // L'événement de clic "l'affichage ou masquage du menu déroulant"
-    icone.addEventListener("click", () => {
+    icone.addEventListener("click", (event) => {
+      event.stopPropagation();
       dropdownMenu.classList.toggle("show");
       inputElement.classList.toggle("show");
+      searchIcon.classList.toggle(
+        "hidden",
+        inputElement.classList.contains("hidden")
+      );
     });
 
-    searchIconListItem.addEventListener("click", () => {
-      dropdownMenu.classList.toggle("show");
-    });
-
-    // Ajoutez cet événement d'entrée pour réagir aux modifications dans la barre de recherche
-    inputElement.addEventListener("input", () => {
+    // Ajout événement d'entrée pour réagir modifications dans barre de recherche
+    inputElement.addEventListener("input", (event) => {
+      event.stopPropagation();
       const searchTerm = inputElement.value.toLowerCase();
       const listItems = dropdownMenu.getElementsByTagName("li");
 
       for (let i = 0; i < listItems.length; i++) {
         const listItem = listItems[i];
         const text = listItem.textContent.toLowerCase();
-
-        if (text.includes(searchTerm)) {
-          listItem.style.display = "block";
-        } else {
-          listItem.style.display = "none";
-        }
+        listItem.style.display = text.includes(searchTerm) ? "block" : "none";
       }
+
+      // Afficher la liste uniquement si la barre de recherche n'est pas vide
+      dropdownMenu.classList.toggle(
+        "show",
+        searchTerm !== "" && !inputElement.classList.contains("hidden")
+      );
+      // Cacher l'icône lorsque l'input est caché
+      searchIcon.classList.toggle(
+        "hidden",
+        inputElement.classList.contains("hidden")
+      );
     });
 
     return container;
@@ -98,6 +106,18 @@ export class DropdownTemplate {
     const div4 = document.createElement("div");
     div4.className = "dropdown dropdown__div";
     div4.textContent = "recette";
+
+    // événement de clic pour incrémenter le compteur
+    div4.addEventListener("click", () => {
+      this.recetteClickCount++;
+      // Mettez à jour le texte de la balise span pour afficher le compteur
+      countElement.textContent = this.recetteClickCount.toString();
+    });
+    //span pour afficher le compteur
+    const countElement = document.createElement("span");
+    countElement.className = "click-count";
+    countElement.textContent = "0";
+    div4.appendChild(countElement);
 
     mainElement.appendChild(div4);
 
