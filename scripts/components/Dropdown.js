cece492efc5549981c5314cc51c14d840b6be5c2
atmani samira <erp.atmanis@gmail.com>
2024-02-15 22:38:54 +0100

    suite_dropdown

--- scripts/components/Dropdown.js
+++ scripts/components/Dropdown.js
@@ -114,11 +114,3 @@ export class Dropdown {
     return dropdown;
   }
 }
-
-// UR THeligh yenou ma3na ougadargh a kressragh,
-//  ur theligh yedi ugadagh a tsba3dadh feli,
-// ur nehader elwahid ma3na tharouchteik dima dakheliw,
-// ur raqegh felak ma3na therouss elhaja degui,
-//  ur kezarara atass dachou amaken snak a-halaya,
-// ur senwigh aka , theligh gouliw,
-// ur thefigadh del moukhiw, ahalet ekhmalerarr, ma3na achourarr ougadarh akroucharg
