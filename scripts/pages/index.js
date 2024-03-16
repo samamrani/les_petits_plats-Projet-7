9485f293df6e232d720ff332b133816fb8b41f54
atmani samira <erp.atmanis@gmail.com>
2024-03-17 00:16:11 +0100

    debut input search

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -3,7 +3,13 @@ import { FiltersTemplate } from "../templates/FiltersTemplate.js";
 import { RecipesTemplate } from "../templates/RecipesTemplate.js";
 
 class App {
-  constructor() {}
+  constructor() {
+    this.ingredients = [];
+    this.appliances = [];
+    this.ustensils = [];
+    this.recipes = null;
+    this.filtersTemplate = null;
+  }
 
   async init() {
     // Récupérer les recettes depuis le fichier JSON
@@ -37,6 +43,9 @@ class App {
     main.appendChild(this.createRecipes());
 
     body.appendChild(main);
+
+    // ???????????????????????
+    this.updateRecipeTags();
   }
 
   getItems() {
@@ -106,6 +115,10 @@ class App {
 
           tag.appendChild(buttonClose);
           tagsSection.appendChild(tag);
+
+          // orsque l'utilisateur sélectionne une recette, j'appelle
+          this.searchRecipes(item);
+          // -------------------
         } else {
           const tag = tagsSection.querySelector(
             `.tag[data-category="${dropdown.dataset.category}"][data-key="${li.dataset.key}"]`
@@ -116,6 +129,9 @@ class App {
         this.updateRecipeTags();
       }
     );
+    // initialiser filtersTemplate
+    this.filtersTemplate = filters;
+
     return filters.getDOM();
   }
 
@@ -139,20 +155,96 @@ class App {
     });
   }
 
-  searchRecipes(searchInput) {
-    const regex = new RegExp(searchInput, "i");
-    const filteredRecipes = this.recipes.filter((recipe) =>
-      regex.test(recipe.name)
-    );
-    const recipesTemplate = new RecipesTemplate(filteredRecipes);
-    const recipesSection = document.querySelector(".recipes");
-    recipesSection.innerHTML = "";
-    recipesSection.appendChild(recipesTemplate.getDOM());
+  // *********************************
+
+  // ,????????????????,,,,,,,,,,,,,,,,,,,,,??????????????????
+  // ?????????????????????????????????????????????????????????|||||||
+  searchRecipes(selected) {
+    console.log(selected);
+
+    const recipesContainer = document.querySelector(".recipes");
+    recipesContainer.innerHTML = "";
+
+    // Filtre les recettes correspondant à la recherche
+    const filteredRecipes = this.recipes.filter((recipe) => {
+      const recipeName = recipe.name
+        .toLowerCase()
+        .includes(selected.toLowerCase());
+
+      const ingredientsName = recipe.ingredients.find((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(selected.toLowerCase())
+      );
+
+      const appliancesName =
+        recipe.appliances &&
+        recipe.appliances.find((appliance) =>
+          appliance.appliance.toLowerCase().includes(selected.toLowerCase())
+        );
+
+      const ustensilsName =
+        recipe.ustensils &&
+        recipe.ustensils.find(
+          (ustensil) =>
+            ustensil.ustensil &&
+            ustensil.ustensil.toLowerCase().includes(selected.toLowerCase())
+        );
+      return recipeName || ingredientsName || appliancesName || ustensilsName;
+    });
+
+    // Mettre à jour recipes avec les recettes filtrées
+    this.recipes = filteredRecipes;
+
+    // Mettre à jour le compteur avec le nombre de recettes
+    this.updateRecipeCount();
   }
 
-  // ,????????????????
+  updateRecipeCount() {
+    const countDiv = document.querySelector("#count");
+    if (countDiv) {
+      countDiv.textContent = `${this.recipes.length} Recette(s)`;
+    }
+
+    // Mettre à jour l'affichage avec les recettes filtrées
+    if (this.recipes.length > 0) {
+      const recipesTemplate = new RecipesTemplate(this.recipes);
+      const recipesSection = document.querySelector(".recipes");
+      recipesSection.innerHTML = "";
+      recipesSection.appendChild(recipesTemplate.getDOM());
+    }
+    // Mettre à jour le nombre de recettes filtrées
+    this.filtersTemplate.updateRecipeCount(this.recipes.length);
+  }
+
+  // //METTRE A JOUR LA LISTE DES RECETTES
   updateRecipeTags() {
-    const tags = document.querySelectorAll("#tags .tag");
+    const tagsSection = document.querySelector("#tags");
+
+    tagsSection.innerHTML = "";
+
+    const filtersContainers = document.querySelectorAll(".filters__container");
+
+    filtersContainers.forEach((container) => {
+      const selectedTags = container.querySelectorAll(".tag");
+
+      selectedTags.forEach((tag) => {
+        const newTag = document.createElement("div");
+        newTag.textContent = tag.textContent;
+        newTag.className = "tag";
+        newTag.dataset.category = tag.dataset.category;
+        newTag.dataset.key = tag.dataset.key;
+
+        // Bouton de fermeture
+        const buttonClose = document.createElement("i");
+        buttonClose.className = "fa-solid fa-xmark icon-close";
+        buttonClose.addEventListener("click", (event) => {
+          event.preventDefault();
+          newTag.remove();
+        });
+
+        newTag.appendChild(buttonClose);
+        tagsSection.appendChild(newTag);
+      });
+    });
   }
 }
 
