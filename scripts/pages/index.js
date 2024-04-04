2c4425384a29038f0c8447644d9947c919195878
atmani samira <erp.atmanis@gmail.com>
2024-04-04 11:27:58 +0200

    update

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -161,61 +161,61 @@ class App {
   }
 
   updateRecipesSearch(searchText) {
+    //   if (searchText.length >= 3) {
+    //     const text = searchText.trim().toLowerCase();
+
+    //     const filteredRecipes = [];
+    //     // traverser toutes les recettes
+    //     for (let i = 0; i < this.recipes.length; i++) {
+    //       const recipe = this.recipes[i];
+    //       const verifyName = recipe.name.toLowerCase().includes(text);
+    //       const verifyDescription = recipe.description
+    //         .toLowerCase()
+    //         .includes(text);
+
+    //       // traverser les ingredients de la recette
+    //       let verifyIngredient = false;
+    //       for (let a = 0; a < recipe.ingredients.length; a++) {
+    //         const item = recipe.ingredients[a];
+    //         if (item.ingredient.toLowerCase().includes(text)) {
+    //           verifyIngredient = true;
+    //           break;
+    //         }
+    //       }
+    //       // ajouter la recette filtree
+    //       if (verifyName || verifyDescription || verifyIngredient) {
+    //         filteredRecipes.push(recipe);
+    //       }
+    //     }
+    //     this.filteredSearch = filteredRecipes;
+    //   } else {
+    //     this.filteredSearch = this.recipes;
+    //   }
+    //   this.updateDisplayRecipes(this.filteredSearch);
+    // }
+
     if (searchText.length >= 3) {
+      // supprim les espaces et passer en miniscule
       const text = searchText.trim().toLowerCase();
 
-      const filteredRecipes = [];
-      // traverser toutes les recettes
-      for (let i = 0; i < this.recipes.length; i++) {
-        const recipe = this.recipes[i];
-        const verifyName = recipe.name.toLowerCase().includes(text);
-        const verifyDescription = recipe.description
-          .toLowerCase()
-          .includes(text);
+      // Filtrer les recettes correspondant à la recherche
+      this.filteredSearch = this.recipes.filter((recipe) => {
+        const nameRecipe = recipe.name.toLowerCase().includes(text);
+        const descRecipe = recipe.description.toLowerCase().includes(text);
 
-        // traverser les ingredients de la recette
-        let verifyIngredient = false;
-        for (let a = 0; a < recipe.ingredients.length; a++) {
-          const item = recipe.ingredients[a];
-          if (item.ingredient.toLowerCase().includes(text)) {
-            verifyIngredient = true;
-            break;
-          }
-        }
-        // ajouter la recette filtree
-        if (verifyName || verifyDescription || verifyIngredient) {
-          filteredRecipes.push(recipe);
-        }
-      }
-      this.filteredSearch = filteredRecipes;
+        // Vérifie ingrédient correspond à la recherche
+        const ingredientRecipe = recipe.ingredients.some((item) =>
+          item.ingredient.toLowerCase().includes(text)
+        );
+
+        return nameRecipe || descRecipe || ingredientRecipe;
+      });
     } else {
+      // inférieure à 3, vide résultats de la recherche
       this.filteredSearch = this.recipes;
     }
     this.updateDisplayRecipes(this.filteredSearch);
   }
-
-  //   if (searchText.length >= 3) {
-  //     // supprim les espaces et passer en miniscule
-  //     const text = searchText.trim().toLowerCase();
-
-  //     // Filtrer les recettes correspondant à la recherche
-  //     this.filteredSearch = this.recipes.filter((recipe) => {
-  //       const nameRecipe = recipe.name.toLowerCase().includes(text);
-  //       const descRecipe = recipe.description.toLowerCase().includes(text);
-
-  //       // Vérifie ingrédient correspond à la recherche
-  //       const ingredientRecipe = recipe.ingredients.some((item) =>
-  //         item.ingredient.toLowerCase().includes(text)
-  //       );
-
-  //       return nameRecipe || descRecipe || ingredientRecipe;
-  //     });
-  //   } else {
-  //     // inférieure à 3, vide résultats de la recherche
-  //     this.filteredSearch = this.recipes;
-  //   }
-  //   this.updateDisplayRecipes(this.filteredSearch);
-  // }
 
   // met à jour la visibilité des recettes en fonction des tags sélectionnés
   updateRecipesTags() {
